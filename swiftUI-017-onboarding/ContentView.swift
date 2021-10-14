a0916931e01c513978be76793ea47686224e4964
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-10-14 10:38:22 -0300

    Initial Commit

---
+++ swiftUI-017-onboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-017-onboarding
+//
+//  Created by Luiz Carlos da Silva Araujo on 14/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI-017-onboarding/swiftUI_017_onboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_017_onboardingApp.swift
+//  swiftUI-017-onboarding
+//
+//  Created by Luiz Carlos da Silva Araujo on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_017_onboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
